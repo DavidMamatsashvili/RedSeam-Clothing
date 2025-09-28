a79681751f83144529c58500c9268b4b48d4ce60
DavidMamatsashvili <mamatsashvilidavid426@gmail.com>
2025-09-29 00:14:11 +0400

    minor updates

--- ProductPageScripts/CheckLogo.js
+++ ProductPageScripts/CheckLogo.js
@@ -1,5 +1,5 @@
 export async function CheckLogo(){
     document.querySelector(".logo-img").addEventListener("click",async()=>{
-        window.location.href="Listing-Page.html";
+        window.location.href="index.html";
     })
 }
